c21c44dc76a51208a720ebb8da7ba8b0bd311dd3
KTPx4 <px4.vnd@gmail.com>
2024-12-02 19:07:09 +0700

    Client: Register account

--- client/app/src/main/java/com/example/ticketbooking/IntroActivity.java
+++ client/app/src/main/java/com/example/ticketbooking/IntroActivity.java
@@ -1,12 +1,63 @@
 package com.example.ticketbooking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IntroActivity extends AppCompatActivity {
+import modules.LocalStorageManager;
+
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnUser, btnStaff;
+    LocalStorageManager localStorageManager ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        localStorageManager = new LocalStorageManager(this);
+        initValue();
+        btnUser = findViewById(R.id.user);
+        btnStaff = findViewById(R.id.staff);
+        btnUser.setOnClickListener(this);
+        btnStaff.setOnClickListener(this);
+
+    }
+    void initValue() // check if has token -> login screen
+    {
+        String loginOption = localStorageManager.getLoginOption();
+        String loginToken = localStorageManager.getLoginToken();
+
+        if(!loginOption.isEmpty())
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra("login-option", loginOption);
+            intent.putExtra("login-token", loginToken);
+            startActivity(intent);
+            this.finish();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id.user)
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra("login-option", "user");
+            localStorageManager.saveLoginOption("user");
+            startActivity(intent);
+        }
+        else if(id == R.id.staff)
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.putExtra("login-option", "staff");
+            localStorageManager.saveLoginOption("staff");
+            startActivity(intent);
+        }
+        this.finish();
     }
 }
--- client/app/src/main/java/com/example/ticketbooking/LoginActivity.java
+++ client/app/src/main/java/com/example/ticketbooking/LoginActivity.java
@@ -1,11 +1,99 @@
 package com.example.ticketbooking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
-public class LoginActivity extends AppCompatActivity {
+
+import services.AccountService;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    private boolean isUser = true;
+    private EditText loginEmail, loginPassword;
+    private String loginToken;
+    AccountService accountService ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        accountService = new AccountService(this);
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        findViewById(R.id.login_button).setOnClickListener(this);
+        findViewById(R.id.login_btn_register).setOnClickListener(this);
+        getFromIntent();
+        checkIsLogin();
+    }
+
+    void getFromIntent()
+    {
+        Intent itent = getIntent();
+        String loginOption = itent.getStringExtra("login-option");
+        loginToken = itent.getStringExtra("login-token");
+
+        if(loginOption.equals("user"))
+        {
+            isUser = true;
+        }
+        else {
+            isUser = false;
+        }
+    }
+
+    void checkIsLogin()
+    {
+        if(loginToken != null && !loginToken.isEmpty())
+        {
+            // check verify token . if true -> main screen
+            // else clear token -> login screen
+
+            if(isUser)
+            {
+                accountService.verifyUser(loginToken, new AccountService.OnRegisterCallback() {
+                    @Override
+                    public void onSuccess(String message) {
+
+                    }
+
+                    @Override
+                    public void onFailure(String error) {
+
+                    }
+                });
+            }
+
+            else{
+                accountService.verifyStaff(loginToken, new AccountService.OnRegisterCallback() {
+
+                    @Override
+                    public void onSuccess(String message) {
+
+                    }
+
+                    @Override
+                    public void onFailure(String error) {
+
+                    }
+                });
+            }
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.login_button)
+        {
+
+        }
+        else if(id == R.id.login_btn_register){
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 }
